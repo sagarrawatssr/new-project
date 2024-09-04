72ea065f16edefa3c2133d7d16c57ba11836f12f
sagar rawat <148690542+sagarrawatssr@users.noreply.github.com>
2024-09-04 22:50:58 +0530

    pattern print
    printing the star patter of design no.

---
+++ pattern coding.py
@@ -0,0 +1,26 @@
+
+a=" "
+b="*"
+def main():
+    n=int(input("enter the no."))
+    for i in range(n):
+        for j in range(n-i-1):
+            print(a,end="")
+        for k in range(2*i+1):
+            print("*",end="")
+        print()
+    g=n-1
+    for i in range(n-1):
+        for j in range(i+1):
+            print(a,end="")
+        for k in range(2*g-1):
+            print("*",end="")
+        print()
+        g-=1
+    ab=input("want to repeat")
+    if ab[0]=='y':
+         main()
+    else:
+        print("thanks for using our service")
+main()
+
